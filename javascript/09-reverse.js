2cc4a505fabc8ec054067802962f1c410451f638
Dovilė <dovile.keparute@gmail.com>
2021-09-07 22:00:06 +0300

    Reverse

--- javascript/09-reverse.js
+++ javascript/09-reverse.js
@@ -28,20 +28,3 @@ console.log(text, '->', reverse);
 
 
 
-// function reverseString(text) {
-//     let reverse = '';
-
-//     for (let i = 0; i < text.length; i++) {
-//         reverse = text[i] + reverse;
-//     }
-
-//     console.log(text, '->', reverse);
-// }
-
-
-// const pirmasZodis = 'Labas rytas';
-// reverseString(pirmasZodis);
-
-// reverseString('Lietuva');
-// reverseString('sedek uzu kedes');
-// reverseString('sula');
